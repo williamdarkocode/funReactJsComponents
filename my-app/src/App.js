931ea30f03e75498bc387a574b704d9f5e27bc0f
William <williamd8323@afsenyc.org>
2019-01-26 18:53:50 -0500

    type

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import WaveyText from './components/wavingTextComponent/WaveyText.js';
 import Typing from './components/typingTextComponent/Typing.js';
 import WavingText from './components/wavingTextComponentWithAnimeJs/WavingText.js';
+import Type from './components/typingComponentWithAnimeJs/Type.js';
 
 import './App.css';
 
@@ -16,7 +17,8 @@ class App extends Component {
       <div className="App">
         {/*<WaveyText rippleText={text}/>*/}
         {/*<Typing wordToType={text}/>*/}
-        <WavingText wavingWord={text} />
+        {/*<WavingText wavingWord={text} />*/}
+        <Type word={text}/>
       </div>
     );
   }
---
+++ my-app/src/components/typingComponentWithAnimeJs/Type.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import './Type.css';
+
+import anime from 'animejs';
+
+export default class Type extends Component {
+  constructor(props) {
+    super(props);
+    this.arr = [];
+    this.divRef = React.createRef();
+      this.state = {
+        txt: this.props.word,
+        wordArr: []
+      }
+  }
+
+
+  componentDidMount() {
+    this.makeArr();
+  }
+  componentDidUpdate() {
+    this.animeRef = anime({
+      targets: this.arr,
+      delay: anime.stagger(200),
+      autoplay: true,
+      loop: true,
+      opacity: {
+        value: 1,
+        easing: 'spring'
+      },
+      easing: 'spring',
+      direction: 'alternate',
+    })
+  }
+
+  makeArr = () => {
+    let tempArr = [];
+    for(let i = 0; i < this.state.txt.length; i++) {
+      tempArr.push(this.state.txt.substring(i, i+1));
+    }
+    this.setState({
+      wordArr: tempArr
+    }, ()=> {
+      console.log('done!');
+    })
+  }
+
+  backSpace = () => {
+    this.animeRef = anime({
+      targets: this.arr,
+      direction: 'reverse',
+      delay: anime.stagger(20),
+      autoplay: true,
+      loop: true,
+    })
+  }
+
+  render() {
+    return(
+      <div id="typeContainer" ref={this.divRef}>
+        {
+          this.state.wordArr.map((i, index) => {
+            return(<span key={index+""} ref={(inp) => {this.arr.push(inp);}} className="typeLetters">{i}</span>)
+          })
+        }
+      </div>
+    )
+  }
+
+}
