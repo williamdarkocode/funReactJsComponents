690b224a9e98d1b7e1bec6418bf6c68963ab4c48
William <williamd8323@afsenyc.org>
2019-06-14 12:42:59 -0400

    Updates

---
+++ precache-manifest.16bbb23c0073520a22446a23af3c400b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "26c7ab095e2d2ff2c6bc",
+    "url": "/myapp/static/js/main.26c7ab09.chunk.js"
+  },
+  {
+    "revision": "4c260f1a5c6f0ce192bb",
+    "url": "/myapp/static/js/1.4c260f1a.chunk.js"
+  },
+  {
+    "revision": "26c7ab095e2d2ff2c6bc",
+    "url": "/myapp/static/css/main.6636e05f.chunk.css"
+  },
+  {
+    "revision": "6ee011e8d9ff0b5319fe2b7716166254",
+    "url": "/myapp/index.html"
+  }
+];
